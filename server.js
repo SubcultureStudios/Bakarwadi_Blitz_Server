d2327c8c6f400fb96f6fe5de228914b40464d482
Soham Aradhye <aradhyesoham@gmail.com>
2024-06-26 16:07:44 +0530

    enable functions

--- server.js
+++ server.js
@@ -27,29 +27,29 @@ app.post('/whatsapp', (req, res) => {
     console.log(from, body)
 
     // Assuming the message contains a predefined command or score check
-    // if (body.toLowerCase().includes('get promocode')) {
-    //     if (promocodes.length > 0) {
-    //         // Get and remove the first promocode from the list
-    //         const promocode = promocodes.shift();
+    if (body.toLowerCase().includes('get promocode')) {
+        if (promocodes.length > 0) {
+            // Get and remove the first promocode from the list
+            const promocode = promocodes.shift();
 
-    //         // Save the remaining promocodes
-    //         fs.writeFileSync('promocodes.json', JSON.stringify(promocodes));
+            // Save the remaining promocodes
+            fs.writeFileSync('promocodes.json', JSON.stringify(promocodes));
 
-    //         // Send promocode back to the user
-    //         client.messages.create({
-    //             from: 'whatsapp:+917558685025', // Your Twilio WhatsApp number
-    //             to: from,
-    //             body: `Congratulations! Your promocode is: ${promocode}`
-    //         }).then(message => console.log(message.sid))
-    //         .catch(error => console.error(error));
+            // Send promocode back to the user
+            client.messages.create({
+                from: 'whatsapp:+917558685025', // Your Twilio WhatsApp number
+                to: from,
+                body: `Congratulations! Your promocode is: ${promocode}`
+            }).then(message => console.log(message.sid))
+            .catch(error => console.error(error));
 
-    //         res.status(200).send('Promocode sent');
-    //     } else {
-    //         res.status(200).send('No more promocodes available');
-    //     }
-    // } else {
-    //     res.status(200).send('Invalid command');
-    // }
+            res.status(200).send('Promocode sent');
+        } else {
+            res.status(200).send('No more promocodes available');
+        }
+    } else {
+        res.status(200).send('Invalid command');
+    }
 
     res.send("Request received")
 });
